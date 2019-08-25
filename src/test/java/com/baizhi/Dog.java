3e3622e8b13db33d8190b59d08546ecbd27a73da
ycj <seeyou_y@qq.com>
2019-08-25 14:41:13 +0800

    修改test类

--- src/test/java/com/baizhi/Dog.java
+++ src/test/java/com/baizhi/Dog.java
@@ -1,7 +1,17 @@
 package com.baizhi;
 
+import java.util.ArrayList;
+
 public class Dog extends Animal {
     public void shout() {
         System.out.println("----dog----------");
+    }
+
+    public void str() {
+        ArrayList<Object> objects = new ArrayList<>();
+        for (Object object : objects) {
+            System.out.println(object);
+            System.out.println(object);
+        }
     }
 }
